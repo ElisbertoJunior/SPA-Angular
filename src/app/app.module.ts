cdf15b256493de7194614edfe23b8036557c6102
Elisberto Junior <elisberto_jr@hotmail.com>
2022-11-12 21:27:36 -0300

    add list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { CompAtributesComponent } from './comp-atributes/comp-atributes.component';
 
+//componentes
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    CompAtributesComponent
   ],
+  // bibliotecas
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/comp-atributes/comp-atributes.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comp-atributes',
+  templateUrl: './comp-atributes.component.html',
+  styleUrls: ['./comp-atributes.component.css']
+})
+export class CompAtributesComponent implements OnInit {
+  style: string = "enable"
+  corFundo: string = "blue"
+  corTexto: string = "white"
+  item: string = ""
+  list: string[] = []
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  toExchange() {
+    if (this.style === "disable") {
+      this.style = "enable"
+    } else {
+      this.style = "disable"
+    }
+  }
+
+  addList() {
+    this.list.push(this.item)
+  }
+
+}
